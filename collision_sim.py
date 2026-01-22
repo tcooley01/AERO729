eaa994dff90193113ca327417e4404a2c072f6dc
Taran Cooley <tcooley@lanl.gov>
2026-01-22 10:21:16 -0700

    got sim somewhat working

--- collision_sim.py
+++ collision_sim.py
@@ -1,44 +1,183 @@
-import numpy as np
-import simpy
-import random as rand
+from typing import Generator, Any, Optional
+
+import random
 import math
-import particle_simulator
+
+import numpy as np
+import simpy as sp
+
+from particle_simulation import (calculate_circle_collision,
+                                 calculate_square_collision,
+                                 calculate_collision_point,
+                                 calulate_reflection)
 
 
 
 class ParticleEnviroment():
     def __init__(self,
-                 num_particles,
-                 init_position,
-                 init_velocity,
-                 circle_radius,
-                 box_length):
+                 num_particles: int,
+                 run_time: float,
+                 init_velocity: str = 'unit_horizontal',
+                 circle_radius: float = 0.2,
+                 box_length: float = 1):
         
         #box edges
         self.edges = [box_length/2, -box_length/2]
+        self.box_length = box_length
         self.sigma = circle_radius
+        self.run_time = run_time
+
 
         #dictionary of particles
+        self.num_particles = num_particles
         self.particles = {}
 
         #simpy env
-        self.env = simpy.Enviroment
+        self.env = sp.Environment()
 
+        #cumulative collisions
+        self.cumulative_collisions = 0
+
+        #populate particles
         for i in range(num_particles):
 
-            if init_velocity == 'unithorizontal'
+            pos = np.random.rand(2) - 0.5
+            while np.linalg.norm(pos, ord = 2) <= self.sigma:
+                pos = np.random.rand(2) - 0.5
+
+            if init_velocity == 'unit_horizontal':
             
-                pos = np.random(1, 2)
-                while np.linalg.norm(pos, ord = 2) <= self.sigma:
-                    pos = np.random(1, 2)
+                particle = {
+                'particle_number'  : i,
+                'velocity'         : np.array([0.1, 0]),
+                'position'         : pos,
+                'number_collisions': 0,
+                'collision_times'  : [] 
+                }
+
+            if init_velocity == 'random':
 
                 particle = {
                 'particle_number'  : i,
-                'velocity'         : np.array([1, 0]),
+                'velocity'         : np.random.rand(2),
                 'position'         : pos,
-                'number_collisions' : 0
-                }
+                'number_collisions': 0,
+                'collision_times'  : []                   
+                }    
 
+            self.particles[f'particle_{i}'] =  particle
 
-            self.particles.update(f'particle_{i}', particle)
+    def _calculate_collision_time(self,
+                                  particle: int
+                                  ) -> Generator[Any, Any, Any]:
+        
+        particle = self.particles[f'particle_{particle}']
+        while True:
 
+            cumulative_wait_time = 0
+
+            time = calculate_circle_collision(
+                position = particle['position'],
+                velocity = particle['velocity'],
+                radius   = self.sigma
+            )
+
+            while time == False:
+
+                square_time = calculate_square_collision(
+                    position = particle['position'],
+                    velocity = particle['velocity'],    
+                    length = self.box_length               
+                )
+
+                collision_point = calculate_collision_point(
+                    position = particle['position'],
+                    velocity = particle['velocity'],
+                    time_to_collision = square_time
+                )
+
+                cumulative_wait_time += square_time
+
+                #collision with x
+                if abs(collision_point[0]) == self.box_length/2:
+
+                    new_position = collision_point - \
+                        2*np.array([collision_point[0], 0])
+                    
+                #collision with y
+                else:
+
+                    new_position = collision_point - \
+                        2*np.array([0, collision_point[1]])
+
+                particle['position'] = new_position
+
+                #recalculate wait time till collision with circle
+                time = calculate_circle_collision(
+                    position = particle['position'],
+                    velocity = particle['velocity'],
+                    radius   = self.sigma
+                )
+                
+                if cumulative_wait_time > self.run_time:
+                    break
+            
+            cumulative_wait_time += time
+
+            if cumulative_wait_time < 0:
+                print(particle['position'])
+                print(np.linalg.norm(particle['position'], ord = 2))
+
+            yield self.env.timeout(cumulative_wait_time)
+
+            new_position = calculate_collision_point(
+                position = particle['position'],
+                velocity = particle['velocity'],
+                time_to_collision = time
+            )
+
+            new_velocity = calulate_reflection(
+                position = new_position,
+                velocity = particle['velocity']
+            )
+
+            particle['position'] = new_position
+            particle['velocity'] = new_velocity
+            particle['number_collisions'] += 1
+            particle['collision_times'].append(self.env.now)
+            self.cumulative_collisions += 1
+
+    def run_sim(self
+                ) -> None:
+
+        for i in range(self.num_particles):
+            particle = self.particles[f'particle_{i}']
+
+            #x-component of velocity is 0
+            if particle['velocity'][0] == 0:
+                
+                #if x-position is within circle
+                if abs(particle['position'][0]) < self.sigma:
+                    self.env.process(self._calculate_collision_time(i))
+
+                #will never intercept 
+                else:
+                    continue
+            
+            #y-component of velocity is 0
+            elif particle['velocity'][1] == 0:
+
+                #if y-position is within circle
+                if abs(particle['position'][1]) < self.sigma:
+                    self.env.process(self._calculate_collision_time(i))
+
+                #will never intercept 
+                else:
+                    continue
+
+            else:
+                self.env.process(self._calculate_collision_time(i))
+
+        self.env.run(until = self.run_time)
+
+        print(f'Sim complete total collisions: {self.cumulative_collisions}')
--- particle_simulation.py
+++ particle_simulation.py
@@ -1,7 +1,7 @@
 import numpy as np
-
-def calculate_circle_collision(position: np.ndarray,
-                               velocity: np.ndarray,
+from numpy.typing import NDArray
+def calculate_circle_collision(position: NDArray,
+                               velocity: NDArray,
                                radius: float
                                )-> float | bool:
     
@@ -12,15 +12,25 @@ def calculate_circle_collision(position: np.ndarray,
     coeffs = [t_2, t, t_0]
 
     roots = np.roots(coeffs)
-
-    if np.iscomplex(roots)[0]:
+    
+    if np.iscomplex(roots)[0] or np.iscomplex(roots)[1]:
         return False
+
+    if roots[0] < 0:
+        return False
+    
 
     return float(np.min(roots))
 
+def calculate_collision_point(position: NDArray,
+                              velocity: NDArray,
+                              time_to_collision: float
+                              ) -> NDArray:
 
-def calculate_square_collision(position: np.ndarray,
-                               velocity: np.ndarray,
+    return velocity*time_to_collision + position
+
+def calculate_square_collision(position: NDArray,
+                               velocity: NDArray,
                                length: float
                                ) -> float:
     x_0 = length/2
@@ -30,15 +40,22 @@ def calculate_square_collision(position: np.ndarray,
     times = np.zeros(2)
 
     if velocity[0] <= 0:
-        time = np.abs((x_1 - position[0])/velocity[0])
-        times[0] = time
+        if velocity[0] == 0:
+            times[0] = np.inf
+        else:
+            time = np.abs((x_1 - position[0])/velocity[0])
+            times[0] = time
     else:
         time = np.abs((x_0 - position[0])/velocity[0])
         times[0] = time    
 
     if velocity[1] <= 0:
-        time = np.abs((y_1 - position[1])/velocity[1])
-        times[1] = time
+        if velocity[1] == 0:
+            times[1] = np.inf
+        else:
+            time = np.abs((y_1 - position[1])/velocity[1])
+            times[1] = time
+            
     else:
         time = np.abs((y_0 - position[1])/velocity[1])
         times[1] = time          
@@ -46,9 +63,9 @@ def calculate_square_collision(position: np.ndarray,
     return float(np.min(times))
 
     
-def calulate_reflection(position: np.ndarray,
-                        velocity: np.ndarray
-                        ) ->  np.ndarray:
+def calulate_reflection(position: NDArray,
+                        velocity: NDArray
+                        ) ->  NDArray:
     
     normal_angle = np.arctan(position[1]/position[0])
     normal_vec = np.array([np.cos(normal_angle), np.sin(normal_angle)])
